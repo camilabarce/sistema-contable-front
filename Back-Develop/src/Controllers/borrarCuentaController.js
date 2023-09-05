c1728e4a66ff569f7dd20cd0dd4958458207c52a
Christian Rincon <christianrinconalem@gmail.com>
2023-09-05 19:19:31 -0300

    Organización de directorios para Frontend y Backend
    Se han creado 2 carpetas para tener organizado el trabajo de cada área y se agregó un script para correr ambos servicios web en simultaneo develop

--- Back-Develop/src/Controllers/borrarCuentaController.js
+++ Back-Develop/src/Controllers/borrarCuentaController.js
@@ -2,10 +2,10 @@ const conexion = require('../Database/conexion');
 
 //Controlador para modificar nombre de la cuenta.
 const borrarCuenta = (req, res) => {
-    const {codigoCuenta, grupoOption, bloqueOption, rubroOption} = req.params;
+    const {codigoCuenta} = req.params;
  
     //Obtenemos los datos de las cuentas desde la base de datos utilizando el stored procedure
-    conexion.query('CALL borrarCuenta(?, ?, ?, ?)', [codigoCuenta, grupoOption, bloqueOption, rubroOption], (error, results) => {
+    conexion.query('CALL borrarCuenta(?)', [codigoCuenta], (error, results) => {
         if (error) throw error;
 
         //Para ver por consola
