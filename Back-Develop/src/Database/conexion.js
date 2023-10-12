0bd48a85effa73f1d95ecef239300d1951b5b28f
TomasBozzano <tomycas10@gmail.com>
2023-10-12 14:19:36 -0300

    Cambios en carpeta y agregado de .env
    * Arreglo del caret por el versionado dentro de Back
    * Modificación del gitignore para que tome todas las carpetas del repositorio
    + Agregado de variables de entorno
    - Resta detalles de la base de DB, en proximo commit

--- Back-Develop/src/Database/conexion.js
+++ Back-Develop/src/Database/conexion.js
@@ -1,19 +1,23 @@
+require('dotenv').config()
 const mysql = require('mysql');
 
-const nombreDeLaBase  = 'sistcontable';
+const host = process.env.HOST_DB
+const user = process.env.USER_DB
+const pass = process.env.PASS_DB
+const db = process.env.DATABASE
 
 const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: nombreDeLaBase
+    host: host,
+    user: user,
+    password: pass,
+    database: db
 });
 
 conexion.connect(function (error) {
     if (error) {
         throw error;
     } else {
-        console.log(`Conexion exitosa a la base de datos: "${nombreDeLaBase}"`);
+        console.log(`Conexion exitosa a la base de datos: "${db}"`);
     }
 });
 
--- Back-Develop/src/app.js
+++ Back-Develop/src/app.js
@@ -1,4 +1,5 @@
 // Configuramos nuestro servidor
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
