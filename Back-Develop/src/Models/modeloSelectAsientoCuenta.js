50524befbccf6f048ae8ce51d65fdd99d3f8f319
Camila Arce <arce.b.camila@gmail.com>
2024-08-12 19:29:18 -0300

    refactor: :art: reestructurar proyecto
    se divide la parte frontend de backend en distintos repositorios

--- Back-Develop/src/Models/modeloSelectAsientoCuenta.js
+++
@@ -1,8 +0,0 @@
-class SelectAsientoCuenta {
-    constructor(id_cuenta, nombre) {
-      this.id_cuenta = id_cuenta;
-      this.nombre = nombre;
-    }
-  }
-  
-  module.exports = SelectAsientoCuenta;
